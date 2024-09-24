a3953389af8285d418961bae23043e9139643f1c
Carlos Bueno <carlos.bueno@csl.lu>
2024-09-24 15:05:31 +0200

    Add ResponsivePage component

---
+++ src/DXBlazorExtensions/ResponsivePage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DXBlazorExtensions
+{
+
+    public class ResponsivePage : ComponentBase
+    {
+        public bool IsXSmallScreen { get; set; }
+
+        public bool IsLoading { get; set; } = true;
+
+        public bool IsVisibleSmallScreen(bool isVisibleSmallScren = true) =>
+            isVisibleSmallScren
+               ? true
+               : !IsXSmallScreen;
+    }
+}
+
